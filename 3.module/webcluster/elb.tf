9e53759bac0f8dde5db78d753829fcb4b123563a
Park-G-s <qkrtk321@naver.com>
2021-02-04 12:02:14 +0900

    필요없는 주석제거

--- 3.module/webcluster/elb.tf
+++ 3.module/webcluster/elb.tf
@@ -21,20 +21,4 @@ resource "aws_elb" "terraform-asg" {
   }
 }
 
-/*resource "aws_security_group" "terraform-elb" {
-  name = "terraform-elb"
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}*/
